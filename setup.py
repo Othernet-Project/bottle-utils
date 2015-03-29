b4fa270686017793ee00f2cb48e18876956dc33e
Branko Vukelic <branko@brankovukelic.com>
2015-03-29 12:25:13 +0200

    This is now a virtual package to bundle all bottle-utils-* together

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 import os
 from setuptools import setup, find_packages
 
-import bottle_utils
-
 def read(fname):
     """ Return content of specified file """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(
-    name = 'bottle-utils',
-    version = bottle_utils.__version__,
-    author = 'Outernet Inc',
-    author_email = 'branko@outernet.is',
-    description = ('Assortment of frequently used utilities for Bottle '
-                   'framework'),
-    license = 'BSD',
-    keywords = 'bottle utils i18n http lazy csrf ajax',
-    url = 'http://outernet-project.github.io/bottle-utils/',
-    packages=find_packages(),
-    long_description="""
-    bottle-utils is a collection of code regularly used in various projects at
-    Outernet Inc (https://www.outernet.is/). It contains methods for working
-    with AJAX, CSRF protection, i18n, and more.
+VERSION = '0.3'
 
-    Documentation can be found at http://outernet-project.github.io/bottle-utils/
-    """,
-    install_requires = [
-        'bottle==0.12.7',
-        'python-dateutil==2.2',
+setup(
+    name='bottle-utils',
+    version=VERSION,
+    author='Outernet Inc',
+    author_email='branko@outernet.is',
+    description=('Assortment of frequently used utilities for Bottle '
+                   'framework'),
+    license='BSD',
+    keywords='bottle utils i18n http lazy csrf ajax',
+    url='http://outernet-project.github.io/bottle-utils/',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=[
+        'bottle-utils-common==%s' % VERSION,
+        'bottle-utils-ajax==%s' % VERSION,
+        'bottle-utils-csrf==%s' % VERSION,
+        'bottle-utils-flash==%s' % VERSION,
+        'bottle-utils-html==%s' % VERSION,
+        'bottle-utils-http==%s' % VERSION,
+        'bottle-utils-i18n==%s' % VERSION,
+        'bottle-utils-lazy==%s' % VERSION,
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
