45937be38acd6971b3812b2ea300e597dd90eb5a
Branko Vukelic <branko@outernet.is>
2015-10-25 12:30:32 +0100

    Improved AJAX documentation

--- bottle_utils/ajax.py
+++ bottle_utils/ajax.py
@@ -1,10 +1,3 @@
-"""
-.. module:: bottle_utils.ajax
-   :synopsis: Utility functions for handling ajax
-
-.. moduleauthor:: Outernet Inc <hello@outernet.is>
-"""
-
 from __future__ import unicode_literals
 
 import functools
@@ -16,10 +9,11 @@ from bottle_utils.common import to_bytes
 
 def ajax_only(func):
     """
-    This decorator simply tests if request ``is_xhr`` and aborts any requests
-    that are not XHR with an HTTP 400 status code. Keep in mind, though, that
-    AJAX header ('X-Requested-With') can be faked, so don't use this decorator
-    as a security measure of any kind.
+    Return HTTP 400 response for all non-XHR requests.
+
+    .. warning::
+        AJAX header ('X-Requested-With') can be faked, so don't use this
+        decorator as a security measure of any kind.
 
     Example::
 
@@ -27,9 +21,6 @@ def ajax_only(func):
         def hidden_from_non_xhr():
             return "Foo!"
 
-    :param func:    request handler
-    :raises:        ``bottle.HTTPResponse``
-    :returns:       wrapped function
     """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -41,7 +32,22 @@ def ajax_only(func):
 
 def roca_view(full, partial, **defaults):
     """
-    Render partal for XHR requests and full template otherwise
+    Render ``partal`` for XHR requests and ``full`` template otherwise. If
+    ``template_func`` keyword argument is found, it is assumed to be a function
+    that renders the template, and is used instead of the default one, which is
+    ``bottle.template()``.
+
+    .. note::
+        To work around issues with Chrome browser (all platforms) when using
+        this decorator in conjunction with HTML5 pushState, the decorator
+        always adds a ``Cache-Control: no-store`` header to partial responses.
+
+    Example::
+
+        @roca_view('page.html', 'fragment.html')
+        def my_roca_handler():
+            return dict()
+
     """
     templ = defaults.pop('template_func', template)
     def decorator(func):
