30f9752d0c68e39b65dc29589e7a22ce406fa028
Ben Ennis <ben@outernet.is>
2016-06-03 13:54:30 -0500

    https is port 443, not 993

--- bottle_utils/html.py
+++ bottle_utils/html.py
@@ -21,7 +21,7 @@ SIZES = 'KMGTP'
 FERR_CLS = 'form-errors'
 FERR_ONE_CLS = 'form-error'
 ERR_CLS = 'field-error'
-HTTP_PORTS = '80 993'.split()
+HTTP_PORTS = '80 443'.split()
 
 
 urlquote = lambda value: quote(to_bytes(value))
